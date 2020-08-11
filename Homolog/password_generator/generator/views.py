7ce3da5b8ded3173d875e7751df47581c6b4173d
njamhour <njjamhour@hotmail.com>
2020-08-11 18:25:43 -0300

    027 - Making random password

--- Homolog/password_generator/generator/views.py
+++ Homolog/password_generator/generator/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 
 def home(request):
     #return HttpResponse('Hello mano')
-    return render(request, 'generator/home.html', {'password':'asdasdasdsa'})
+    return render(request, 'generator/home.html')
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefgijklmnopqrsuvwxyz')
+
+    length = 10
+
+    thepassword = ''
+
+    for x in range(length):
+        thepassword += random.choice(characters)
+
+
+    return render(request, 'generator/password.html', {'password':thepassword})
