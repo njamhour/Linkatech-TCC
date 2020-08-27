6ba1b9acdce33a1d25c4377b937b15365946fa60
njamhour <njjamhour@hotmail.com>
2020-08-27 00:37:19 -0300

    API funcional

--- Homolog/myapi/myapi/urls.py
+++ Homolog/myapi/myapi/urls.py
@@ -1,4 +1,4 @@
-"""HOME URL Configuration
+"""myapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.HelloView.as_view(), name='hello'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth')
+    
 ]
